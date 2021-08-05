1c2c6b6d0e6bf5e26233d7a0ce21473b76364557
Larbi Sahli <larbisahli1905@gmail.com>
2021-08-06 00:54:12 +0100

    allow origin cors admin.dropgala.com

--- src/server.js
+++ src/server.js
@@ -71,11 +71,11 @@ app.use(speedLimiter);
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,POST');
+  res.header('Access-Control-Allow-Origin', 'https://admin.dropgala.com');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
   res.header(
     'Access-Control-Allow-Headers',
-    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, multipart/form-data'
   );
   next();
 });
